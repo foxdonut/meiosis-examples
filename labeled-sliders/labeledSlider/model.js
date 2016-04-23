1c5949c57c150c3abc144cacb98ff5e6222491ce
foxdonut <fdaoud@fastmail.com>
2016-04-22 20:55:05 -0400

    Implement labeled-sliders based on Meiosis 0.2.2.

--- labeled-sliders/labeledSlider/model.js
+++ labeled-sliders/labeledSlider/model.js
@@ -1,11 +1,5 @@
-import { assoc } from "ramda";
-
 const update = Action => (model, action) => Action.case({
-  Update: (index, value) => {
-    // FIXME: need lens function update here.
-    model.measurements[index] = assoc("value", value, model.measurements[index]);
-    return model;
-  }
+  Update: (index, value) => ({ index, value })
 }, action);
 
 export { update };
--- labeled-sliders/sliderContainer/main.js
+++ labeled-sliders/sliderContainer/main.js
@@ -1,3 +1,4 @@
+import { assoc, merge } from "ramda";
 import { Action } from "./actions";
 import { initialModel, update } from "./model";
 import view from "./view.jsx";
@@ -9,8 +10,17 @@ const createSliderContainer = createComponent => {
 
   return createComponent({
     initialModel,
+    view: view(LabeledSlider),
     update: update(Action),
-    view: view(LabeledSlider)
+    pipeline: (model, update) => {
+      if (parseInt(update.index, 10) >= 0) {
+        model.measurements[update.index] = assoc("value", update.value, model.measurements[update.index]);
+      }
+      else {
+        model = merge(model, update);
+      }
+      return model;
+    }
   });
 };
 
