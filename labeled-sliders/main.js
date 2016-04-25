67e44e8e604821eb65dad09a78863f6f88358f56
foxdonut <fdaoud@fastmail.com>
2016-04-25 18:50:13 -0400

    Using meiosis render helpers.

--- labeled-sliders/main.js
+++ labeled-sliders/main.js
@@ -1,20 +1,8 @@
 import meiosis from "meiosis";
 import snabbdom from "snabbdom";
+import { renderAdapterIdConfig } from "meiosis-snabbdom";
 import createMain from "./labeledSliders";
 
-const patch = snabbdom.init([
-  require("snabbdom/modules/attributes"),
-  require("snabbdom/modules/class"),
-  require("snabbdom/modules/props"),
-  require("snabbdom/modules/style"),
-  require("snabbdom/modules/eventlisteners")
-]);
-
-let element = document.getElementById("app");
-
-const render = view => element = patch(element, view);
-const adapters = { render };
-const Meiosis = meiosis(adapters);
+const Meiosis = meiosis(renderAdapterIdConfig(snabbdom, "app"));
 
 createMain(Meiosis);
-
--- todo-list/main.js
+++ todo-list/main.js
@@ -1,10 +1,8 @@
 import meiosis from "meiosis";
 import { render } from "react-dom";
+import meiosisReact from "meiosis-react";
 import createMain from "./todoMain.jsx";
 
-const element = document.getElementById("app");
-const adapters = { render: view => render(view, element) };
-const Meiosis = meiosis(adapters);
+const Meiosis = meiosis(meiosisReact.intoId(render, "app"));
 
 createMain(Meiosis);
-
