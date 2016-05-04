4972f97482aa7861ec1edd08da4e2ac7992b6754
foxdonut <fdaoud@fastmail.com>
2016-05-03 20:31:12 -0400

    Change hello world example. Placeholders for todomvc example.

--- hello-meiosis/main.js
+++ hello-meiosis/main.js
@@ -1,19 +1,31 @@
 /*global meiosis, meiosisVanillaJs*/
-// FIXME
 var Meiosis = meiosis(meiosisVanillaJs.intoId("app"));
 
 var createComponent = Meiosis.createComponent;
 
 var Main = createComponent({
-  initialModel: {name: "Meiosis"},
+  initialModel: { counter: 0 },
+  actions: function(sendUpdate) {
+    return {
+      inc: function() {
+        sendUpdate({ add: 1 });
+      },
+      decr: function() {
+        sendUpdate({ add: -1 });
+      }
+    };
+  },
+  receiveUpdate: function(model, update) {
+    return { counter: model.counter + update.add };
+  },
   view: function(model) {
-    return "<input id='user' value='" + model.name + "'>" +
-      "<div>Hello, " + model.name + "</div>";
+    return "<div><span>Counter: " + model.counter + "</span></div>" +
+      "<div><button id='inc'>+</button> <button id='decr'>-</button></div>";
   },
   ready: function(actions) {
-    document.body.addEventListener("change", function(evt) {
-      if (evt.target.tagName.toLowerCase() === "input") {
-        actions.sendUpdate({ name: evt.target.value });
+    document.body.addEventListener("click", function(evt) {
+      if (evt.target.tagName.toLowerCase() === "button") {
+        actions[evt.target.id]();
       }
     });
   }
