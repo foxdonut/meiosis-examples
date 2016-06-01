9a28be3cc681c3da5d9dbc9109f260038b15828b
foxdonut <fdaoud@fastmail.com>
2016-06-01 18:25:15 -0400

    Better viewModel.

--- examples/todomvc/common/viewModel.js
+++ examples/todomvc/common/viewModel.js
@@ -1,6 +1,7 @@
 /*global window*/
 (function(ref) {
   ref.viewModel = function(model) {
+    var viewModel = Object.assign({}, model);
     var by = model.filter;
     var completed = by === "completed";
 
@@ -10,18 +11,18 @@
     function() {
       return true;
     };
-    model.todos = model.todos.filter(filterBy);
+    viewModel.todos = model.todos.filter(filterBy);
 
     var notCompleted = function(todo) { return !todo.completed; };
-    var itemsLeft = model.todos.filter(notCompleted).length;
-    model.itemsLeftText = model.todos.length > 0 ?
+    var itemsLeft = viewModel.todos.filter(notCompleted).length;
+    viewModel.itemsLeftText = viewModel.todos.length > 0 ?
       (String(itemsLeft) + " item" + (itemsLeft === 1 ? "" : "s") + " left") : "";
-    model.clearCompleted = (model.todos.length - itemsLeft) > 0;
+    viewModel.clearCompleted = (viewModel.todos.length - itemsLeft) > 0;
 
-    model.allSelected = model.filter === "all";
-    model.activeSelected = model.filter === "active";
-    model.completedSelected = model.filter === "completed";
+    viewModel.allSelected = model.filter === "all";
+    viewModel.activeSelected = model.filter === "active";
+    viewModel.completedSelected = model.filter === "completed";
 
-    return model;
+    return viewModel;
   };
 })(window);
