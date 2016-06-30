818223ac89f515d33c516b16ef65b9ffe2caa3e8
foxdonut <fdaoud@fastmail.com>
2016-06-30 08:12:51 -0400

    Working on React todomvc example.

--- examples/todomvc/react/root/view.jsx
+++ examples/todomvc/react/root/view.jsx
@@ -1,21 +1,47 @@
-/*global React */
-(function(ref) {
-  var info = (
-    <footer className="info">
-      <p>Double-click to edit a todo</p>
-      <p>Meiosis - React - Created by <a href="http://twitter.com/foxdonut00">foxdonut00</a></p>
-      <p>Part of <a href="http://todomvc.com">TodoMVC</a></p>
-    </footer>
-  );
+/*global define, exports, module, require*/
 
-  ref.root = ref.root || {};
+// This boilerplate is to support running this code with either, just the browser, or RequireJS,
+// or node.js / npm (browserify, webpack, etc.) Do not think this boilerplate is necessary to run
+// Meiosis. It is for convenience to be able to run the example with your preferred module system.
+(function(root, moduleName, depNames, depVars, factory) {
+  if (typeof define === "function" && define.amd) {
+    define(depNames, factory);
+  }
+  else if (typeof exports === "object") {
+    var requires = depNames.map(function(depName) {
+      return require(depName);
+    });
+    module.exports = factory.apply(root, requires);
+  }
+  else {
+    var vars = depVars.map(function(depVar) {
+      return root[depVar];
+    });
+    root[moduleName] = factory.apply(root, vars);
+  }
+}(this, // ^^ the code above is boilerplate. the "real" code starts below. vv
+  "rootView",
+  ["react"],
+  ["React"],
 
-  ref.root.view = function(todoapp) {
-    return (
-      <div>
-        {todoapp}
-        {info}
-      </div>
+  function(React) {
+    var info = (
+      <footer className="info">
+        <p>Double-click to edit a todo</p>
+        <p>Meiosis - React - Created by <a href="http://twitter.com/foxdonut00">foxdonut00</a></p>
+        <p>Part of <a href="http://todomvc.com">TodoMVC</a></p>
+      </footer>
     );
-  };
-})(window);
+
+    return function(todoapp) {
+      return function(model) {
+        return (
+          <div>
+            {todoapp(model)}
+            {info}
+          </div>
+        );
+      };
+    };
+  }
+));
