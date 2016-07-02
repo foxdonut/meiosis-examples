62e953f87d9b0089d0ee819a67ce7ecabba59ac8
foxdonut <fdaoud@fastmail.com>
2016-07-02 18:44:50 -0400

    Reorder UMD

--- examples/todomvc/react/app.jsx
+++ examples/todomvc/react/app.jsx
@@ -4,14 +4,14 @@
 // or node.js / npm (browserify, webpack, etc.) Do not think this boilerplate is necessary to run
 // Meiosis. It is for convenience to be able to run the example with your preferred module system.
 (function(root, moduleName, depNames, depVars, factory) {
-  if (typeof define === "function" && define.amd) {
-    define(depNames, factory);
-  }
-  else if (typeof exports === "object") {
+  if (typeof exports === "object") {
     var requires = depNames.map(function(depName) {
       return require(depName);
     });
     module.exports = factory.apply(root, requires);
+  }
+  else if (typeof define === "function" && define.amd) {
+    define(depNames, factory);
   }
   else {
     var vars = depVars.map(function(depVar) {
