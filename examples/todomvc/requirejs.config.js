f47bfd432be6ca8332f417e6c82bfd61d7f7db2c
foxdonut <fdaoud@fastmail.com>
2016-07-03 18:39:42 -0400

    Working on todomvc example consolidation.

--- examples/todomvc/requirejs.config.js
+++ examples/todomvc/requirejs.config.js
@@ -7,7 +7,7 @@ requirejs.config({
     "meiosis-mithril": "/public/lib/meiosis-mithril.min",
     "meiosis-react": "/public/lib/meiosis-react.min",
     "meiosis-snabbdom": "/public/lib/meiosis-snabbdom.min",
-    "meiosis-vanillaJs": "/public/lib/meiosis-vanillajs.min",
+    "meiosis-vanillajs": "/public/lib/meiosis-vanillajs.min",
     "meiosis-tracer": "/public/lib/meiosis-tracer.min",
     "mithril": "/public/lib/mithril.min",
     "union-type": "/public/lib/union-type"
