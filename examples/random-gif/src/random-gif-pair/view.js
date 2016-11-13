5eb25873c9f1ecee3accf350fdfba7f9bf72b243
foxdonut <fdaoud@fastmail.com>
2016-11-13 16:01:10 -0500

    Remove extraneous div.

--- examples/random-gif/src/random-gif-pair/view.js
+++ examples/random-gif/src/random-gif-pair/view.js
@@ -3,12 +3,10 @@ import m from "mithril";
 export function view(components) {
   return function(state, actions) {
     return m("div", [
-      m("div", [
-        m("div", { style: "display: inline-block" },
-          components.randomGifFirst.view(state.randomGifFirst, actions.randomGifFirst)),
-        m("div", { style: "display: inline-block" },
-          components.randomGifSecond.view(state.randomGifSecond, actions.randomGifSecond))
-      ])
+      m("div", { style: "display: inline-block" },
+        components.randomGifFirst.view(state.randomGifFirst, actions.randomGifFirst)),
+      m("div", { style: "display: inline-block" },
+        components.randomGifSecond.view(state.randomGifSecond, actions.randomGifSecond))
     ]);
   };
 }
