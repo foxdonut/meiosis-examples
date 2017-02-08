e37b1a630f8c144d4f4f0db55485dbcd8c9885cc
foxdonut <fdaoud@fastmail.com>
2017-02-07 20:52:31 -0500

    WIP

--- examples/random-gif/src/random-gif-list/actions.js
+++ examples/random-gif/src/random-gif-list/actions.js
@@ -1,5 +1,6 @@
-import { on, stream } from "meiosis";
+import { map, on, stream } from "meiosis";
 import * as C from "./constants";
+import { randomGif } from "../random-gif";
 
 export const intents = {
   add: stream(),
@@ -10,3 +11,18 @@ export const createActions = ({ propose }) => {
   on(() => propose({ type: C.GIF_LIST_ADD }), intents.add);
   on(id => propose({ type: C.GIF_LIST_REMOVE, id }), intents.remove);
 };
+
+export const add = map(() => model => {
+  const randomGifModel = randomGif.initialModel();
+  model.randomGifIds.push(randomGifModel.id);
+  model.randomGifsById[randomGifModel.id] = randomGifModel;
+  return model;
+}, intents.add);
+
+export const remove = map(id => model => {
+  delete model.randomGifsById[id];
+  model.randomGifIds.splice(model.randomGifIds.indexOf(id), 1);
+  return model;
+}, intents.remove);
+
+//model.randomGifIds.forEach(id => randomGif.receive(model.randomGifsById[id], proposal));
