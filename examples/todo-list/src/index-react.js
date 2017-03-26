64e03b53609348550f4c173ced13c1515c7ed286
foxdonut <fdaoud@fastmail.com>
2017-03-26 17:30:45 -0400

    Working on event system.

--- examples/todo-list/src/index-react.js
+++ examples/todo-list/src/index-react.js
@@ -19,25 +19,35 @@ const modelChanges = flyd.stream();
 const model = flyd.scan(applyModelChange, initialModel, modelChanges);
 
 const eventStream = flyd.stream();
-const events = createEvents(eventStream, {
-  form: [
-    "editTodo",
-    "saveTodoFailure",
-    "saveTodoStart",
-    "saveTodoSuccess",
-  ],
-  list: [
-    "editTodo",
-    "error",
-    "pleaseWait",
-    "todoList",
-    "updateTodo"
-  ]
-}, {
-  "form.saveTodoFailure": ["list.error"],
-  "form.saveTodoStart": ["list.pleaseWait"],
-  "form.saveTodoSuccess": ["list.updateTodo"],
-  "list.editTodo": ["form.editTodo"]
+const events = createEvents({
+  eventStream,
+  emit: {
+    form: [
+      "saveTodoFailure",
+      "saveTodoStart",
+      "saveTodoSuccess",
+    ],
+    list: [
+      "editTodo"
+    ]
+  },
+  listen: {
+    form: [
+      "editTodo"
+    ],
+    list: [
+      "error",
+      "pleaseWait",
+      "todoList",
+      "updateTodo"
+    ]
+  },
+  connect: {
+    "form.saveTodoFailure": ["list.error"],
+    "form.saveTodoStart": ["list.pleaseWait"],
+    "form.saveTodoSuccess": ["list.updateTodo"],
+    "list.editTodo": ["form.editTodo"]
+  }
 });
 
 trace({ streamLibrary: flyd, modelChanges, streams: [ model, eventStream ]});
