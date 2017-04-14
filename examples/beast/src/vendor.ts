becd57d445dede3e0e2ca61b52d5fbf65a254656
foxdonut <fdaoud@fastmail.com>
2017-04-14 19:27:41 -0400

    Angular (beast) startup

---
+++ examples/beast/src/vendor.ts
@@ -0,0 +1,13 @@
+// Angular
+import "@angular/common";
+import "@angular/core";
+import "@angular/http";
+import "@angular/platform-browser";
+import "@angular/platform-browser-dynamic";
+import "@angular/router";
+
+// RxJS
+import "rxjs";
+
+// Other vendors for example jQuery, Lodash or Bootstrap
+// You can import js, ts, css, sass, ...
