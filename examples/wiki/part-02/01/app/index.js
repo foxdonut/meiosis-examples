2f83752990d1e2f423eab9cfefd1e7d27c47aa4e
foxdonut <fdaoud@fastmail.com>
2017-09-10 16:05:43 -0400

    Update examples.

---
+++ examples/wiki/part-02/01/app/index.js
@@ -0,0 +1,18 @@
+import { createView } from "./view";
+import { createTemperature } from "../temperature";
+import { nest } from "../util/nest";
+
+export const createApp = update => {
+  const components = {
+    air: createTemperature(nest(update, "air")),
+    water: createTemperature(nest(update, "water"))
+  };
+
+  return {
+    model: () => ({
+      air: components.air.model(),
+      water: components.water.model()
+    }),
+    view: createView(components)
+  };
+};
---
+++ examples/wiki/part-02/02/index.js
@@ -0,0 +1,17 @@
+/* global ReactDOM */
+import flyd from "flyd";
+
+import { createApp } from "./app";
+
+const update = flyd.stream();
+const app = createApp(update);
+const initialModel = app.model();
+const applyUpdate = (model, modelUpdate) => modelUpdate(model);
+const models = flyd.scan(applyUpdate, initialModel, update);
+
+const element = document.getElementById("app");
+models.map(model => ReactDOM.render(app.view(model), element));
+
+// Only for using Meiosis Tracer in development.
+import { trace } from "meiosis";
+trace({ update, dataStreams: [ models ]});
