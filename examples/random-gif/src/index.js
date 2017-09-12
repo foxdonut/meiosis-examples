9f9a1777755f3d9b8738b477b8e375fa2bded829
foxdonut <fdaoud@fastmail.com>
2017-09-11 21:57:16 -0400

    Updating examples.

--- examples/random-gif/src/index.js
+++ examples/random-gif/src/index.js
@@ -5,12 +5,11 @@ import { createApp } from "./app";
 import { createIncrement } from "./increment";
 
 const update = stream();
+const event = stream();
 const app = createApp(event)(update);
 const initialModel = app.model();
 const applyUpdate = (model, modelUpdate) => modelUpdate(model);
 const models = stream.scan(applyUpdate, initialModel, update);
-
-const event = stream();
 
 createIncrement(event)(update);
 
