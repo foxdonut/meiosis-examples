a09274969bb555edbbce1e8e92d6436984a90c57
foxdonut <fdaoud@fastmail.com>
2017-09-16 13:23:42 -0400

    Update examples.

---
+++ examples/wiki/part-03/02/temperature/actions.js
@@ -0,0 +1,19 @@
+import R from "ramda";
+
+export const createActions = update => ({
+  editDate: evt => update(R.assoc("date", evt.target.value)),
+
+  increase: amount => () => update(R.over(R.lensProp("value"), R.add(amount))),
+
+  changeUnits: () => update(model =>
+    R.call((model.units === "C")
+      ? R.compose(
+          R.assoc("units", "F"),
+          R.assoc("value", Math.round( model.value * 9 / 5 + 32 ))
+        )
+      : R.compose(
+          R.assoc("units", "C"),
+          R.assoc("value", Math.round( (model.value - 32) / 9 * 5 ))
+      ), model)
+  )
+});
---
+++ examples/wiki/part-03/02/util/nest.js
@@ -0,0 +1,43 @@
+import R from "ramda";
+
+export const nestUpdate = (update, path) =>
+  modelUpdate => update(R.over(R.lensProp(path), modelUpdate));
+
+const nestFunction = (path, fn) =>
+  model => fn(R.prop(path, model));
+
+const noNest = (path, fn) => fn;
+
+const defaultConfig = {
+  view: nestFunction
+};
+
+export const nestComponent = (create, update, path, customConfig) => {
+  const config = customConfig
+    ? Object.assign({}, defaultConfig, customConfig)
+    : defaultConfig;
+
+  const component = create(nestUpdate(update, path));
+  const nestedComponent = Object.keys(component).reduce((result, key) => {
+    const modifier = R.defaultTo(noNest, config[key]);
+    return R.assoc(key, modifier(path, component[key]), result);
+  }, {});
+  return nestedComponent;
+};
+
+export const createComponents = (update, componentStructure, customConfig) =>
+  Object.keys(componentStructure).reduce((result, path) =>
+    R.assoc(
+      path,
+      nestComponent(R.prop(path, componentStructure), update, path, customConfig),
+      result
+    ), {});
+
+export const combineComponents = (components, componentKey) =>
+  model => Object.keys(components).reduce((result, key) => {
+    const target = R.path([key, componentKey], components);
+    if (target) {
+      result = R.assoc(key, target(model), result);
+    }
+    return result;
+  }, {});
