4441962ab1c65dab2638578854c4bb08ad8a0dda
foxdonut <fdaoud@fastmail.com>
2018-03-15 22:42:06 -0400

    Working on Meiosis tutorial.

--- examples/tutorial/images/generate_one_diagram.sh
+++ examples/tutorial/images/generate_one_diagram.sh
@@ -7,8 +7,8 @@ SRC=$1
 DEST=$(echo $SRC | sed -e "s/\.dot$/.$EXT/") && \
 PNG=$(echo $DEST | sed -e "s/\.$EXT/.png/") && \
 echo $SRC '->' $DEST &&\
-dot -T$EXT -Nshape=record -Nmargin=0.05 -Nfontsize=10pt -Nfontname=Consolas -Efontsize=10pt -Efontname=helvetica -Ecolor=maroon -Efontcolor=navy -Gnodesep=0.2 -o $DEST $SRC && \
-svgexport $DEST $PNG
+dot -T$EXT -Nshape=record -Nmargin=0.05 -Nfontsize=12pt -Nfontname=Courier -Efontsize=12pt -Efontname=helvetica -Ecolor=maroon -Efontcolor=navy -Gnodesep=0.2 -o $DEST $SRC && \
+# svgexport $DEST $PNG
 # echo 'Cleanup..' && \
 #  rm $TEMPSVG && \
 echo 'Done.'
