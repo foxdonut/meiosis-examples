97232351d43b25df9cf579b58c405408b1051ad0
foxdonut <fdaoud@fastmail.com>
2018-05-03 07:56:16 -0400

    Update SAM example.

--- examples/sandbox/sam/src/index.js
+++ examples/sandbox/sam/src/index.js
@@ -3,23 +3,19 @@ import { render } from "lit-html/lib/lit-extended";
 
 import { createTemperature } from "./temperature";
 
-// Only for using Meiosis Tracer in development.
-import { trace } from "meiosis";
-import meiosisTracer from "meiosis-tracer";
-
 const update = flyd.stream();
 const app = createTemperature(update);
 
-const models = flyd.scan(
-  (model, modelUpdate) => modelUpdate(model),
-  app.model(),
-  update
-);
+const models = flyd.scan((model, func) => func(model),
+  app.model(), update);
 
 const element = document.getElementById("app");
 const states = models.map(app.state);
 states.map(state => render(app.view(state), element));
 
 // Only for using Meiosis Tracer in development.
-trace({ update, dataStreams: [ states ]});
+import { trace } from "meiosis";
+import meiosisTracer from "meiosis-tracer";
+
+trace({ update, dataStreams: [ models, states ]});
 meiosisTracer({ selector: "#tracer" });
--- examples/sandbox/sam/src/temperature/state.js
+++ examples/sandbox/sam/src/temperature/state.js
@@ -1,10 +1,11 @@
 export const state = model => {
+  const result = Object.assign({}, model);
   let temp = model.value;
 
   if (model.units === "F") {
     temp = Math.round((temp - 32) * 5 / 9);
   }
-  model.comment = (temp < 18) ? "COLD!" : (temp > 25) ? "HOT!" : "";
+  result.comment = (temp < 18) ? "COLD!" : (temp > 25) ? "HOT!" : "";
 
-  return model;
+  return result;
 };
