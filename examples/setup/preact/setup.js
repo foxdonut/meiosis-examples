176a1996b7f3851a01bcbe85837be33309a72ff4
foxdonut <fdaoud@fastmail.com>
2018-07-07 16:35:20 -0400

    Working on seview examples.

--- examples/setup/preact/setup.js
+++ examples/setup/preact/setup.js
@@ -2,11 +2,20 @@ import preact from "preact";
 import { setup } from "../common";
 import { mapKeys, sv } from "seview";
 
-const h = sv(mapKeys({
+const mk = mapKeys({
   tag: "nodeName",
   attrs: "attributes",
   children: "children"
-}))
+});
+
+const h = sv(node => {
+  const attrs = node.attrs || {};
+  if (attrs.innerHTML) {
+    attrs.dangerouslySetInnerHTML = { __html: attrs.innerHTML };
+    delete attrs.innerHTML;
+  }
+  return mk(node);
+})
 
 export const setupRender = () =>
   (view, element) => preact.render(h(view), element, element.lastElementChild);
