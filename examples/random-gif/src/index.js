cc0835c7d62c46d4cae507f8a96050e5ea9e356d
foxdonut <fdaoud@fastmail.com>
2018-08-04 10:52:56 -0400

    updating examples

--- examples/random-gif/src/index.js
+++ examples/random-gif/src/index.js
@@ -16,9 +16,21 @@ states.map(state => m.render(element, h(app.view(state))))
 // Only for using Meiosis Tracer in development.
 const meiosisTracer = require("meiosis-tracer")
 const { signals } = require("./random-gif/signals")
-meiosisTracer({ selector: "#tracer", autoSend: true, streams: [
+meiosisTracer({ selector: "#tracer", autoSend: false, streams: [
 //meiosisTracer({ streams: [
   { label: "models", stream: models, hist: false },
   { label: "states", stream: states, hide: true },
   { label: "newGif", stream: signals.newGif }
-], rows: 12 })
+], rows: 9, listen: (stream, fn) => {
+  console.log("listen")
+  stream.map(fn)
+}, emit: (stream, value) => {
+  console.log("emit:", value)
+  stream(value)
+}, stringify: obj => {
+  console.log("stringify")
+  return JSON.stringify(obj, null, 2)
+}, parse: str => {
+  console.log("parse")
+  return JSON.parse(str)
+} })
