aede63b29fc734ecad8ae54147177da8f63590fc
foxdonut <fdaoud@fastmail.com>
2018-10-02 09:16:13 -0400

    Working on improving todo-list example.

--- examples/todo-list/src/todos/todoForm/actions.js
+++ examples/todo-list/src/todos/todoForm/actions.js
@@ -2,20 +2,18 @@ import R from "ramda";
 
 import { validateModel } from "./validation";
 
-export const actions = ({ update, actions }) => {
-  return {
-    editingTodo: (id, field, value) => update(R.over(R.lensProp(id),
-      model => R.assoc("todo", R.assoc(field, value, model.todo), model))),
+export const actions = ({ update, actions }) => ({
+  editingTodo: (id, field, value) => update(R.over(R.lensProp(id),
+    model => R.assoc("todo", R.assoc(field, value, model.todo), model))),
 
-    onSaveTodo: (id, todo) => {
-      const validationErrors = validateModel(todo);
+  onSaveTodo: (id, todo) => {
+    const validationErrors = validateModel(todo);
 
-      if (Object.keys(validationErrors).length === 0) {
-        actions.saveTodo(id, todo);
-      }
-      else {
-        update(R.over(R.lensProp(id), R.assoc("validationErrors", validationErrors)));
-      }
+    if (Object.keys(validationErrors).length === 0) {
+      actions.saveTodo(id, todo);
     }
-  };
-};
+    else {
+      update(R.over(R.lensProp(id), R.assoc("validationErrors", validationErrors)));
+    }
+  }
+});
