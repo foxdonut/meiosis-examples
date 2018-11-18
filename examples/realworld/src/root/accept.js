d702cad071f80173830d9325a3e277d3061419cb
foxdonut <fdaoud@fastmail.com>
2018-11-18 09:20:44 -0500

    Consolidate settings accept

--- examples/realworld/src/root/accept.js
+++
@@ -1,8 +0,0 @@
-import { HomePage, SettingsPage, navigateTo } from "../util/router"
-
-export const accept = (model, patch) => {
-  if (patch.pageId === SettingsPage && !model.user) {
-    return navigateTo(HomePage)
-  }
-  return patch
-}
