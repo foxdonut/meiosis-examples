114f5deb03a69cea5ecd8e14dea88a085be1ec25
foxdonut <fdaoud@fastmail.com>
2019-07-29 05:19:44 -0400

    Working on realworld example.

--- examples/realworld/src/util/view.js
+++ examples/realworld/src/util/view.js
@@ -5,11 +5,18 @@ import { h } from "seview/mithril"
 export const render = element => view => m.render(element, h(view))
 end mithril */
 
-/* react */
+/* preact */
+import preact from "preact"
+import { h } from "seview/preact"
+
+export const render = element => view => preact.render(h(view), element, element.lastElementChild)
+/* end preact */
+
+/* react
 import ReactDOM from "react-dom"
 import { h } from "seview/react"
 
 export const render = element => view => ReactDOM.render(h(view), element)
-/* end react */
+end react */
 
 export const defaultImage = "/assets/smiley-cyrus.jpg"
