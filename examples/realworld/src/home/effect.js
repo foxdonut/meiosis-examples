e9cf94223066eee970c11dfa5c7e4251e1f535e6
foxdonut <fdaoud@fastmail.com>
2020-05-13 20:41:29 -0400

    working on realworld example

--- examples/realworld/src/home/effect.js
+++ examples/realworld/src/home/effect.js
@@ -1,13 +1,13 @@
 import { articlesApi, loadArticlesAndTags } from "../services"
 import { Route } from "../router"
-import { pick } from "../util/fp"
+import { get, pick } from "../util/fp"
 import { getArticlesFilter } from "../util/filter"
 
 export const Effect = update => state => {
   if (state.route.page === Route.Home && state.loading) {
     const filter = getArticlesFilter(state.route)
 
-    state.route.params.feed
+    get(state, ["route", "params", "queryParams", "feed"])
       ? articlesApi
           .getFeed(pick(["limit", "offset"], filter))
           .then(data => update([data, { loading: false }]))
--- examples/realworld/src/home/service.js
+++ examples/realworld/src/home/service.js
@@ -1,16 +1,19 @@
 import { Route } from "../router"
-import { get, identity, when } from "../util/fp"
-
-const getOffset = route =>
-  when(identity, parseInt, get(route, ["params", "queryParams", "offset"])) || 0
+import { identity, when } from "../util/fp"
 
 export const service = state => {
   if (state.route.page === Route.Home) {
-    const offset = getOffset(state.route)
-    const tag = get(state, ["route", "params", "queryParams", "tag"])
+    const queryParams = state.route.params.queryParams || {}
+    const offset = when(identity, parseInt, queryParams.offset) || 0
+    const tag = queryParams.tag
+    const feed = queryParams.feed
 
-    if (state.previous.offset !== offset || state.previous.tag !== tag) {
-      return { loading: true, previous: { offset, tag } }
+    if (
+      state.previous.offset !== offset ||
+      state.previous.tag !== tag ||
+      state.previous.feed !== feed
+    ) {
+      return { loading: true, previous: { offset, tag, feed } }
     }
   }
 }
