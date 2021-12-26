9bd15af64e95021548ef301a724f2752c856d70f
foxdonut <fdaoud@fastmail.com>
2021-12-26 10:39:26 -0500

    using Meiosis Cell in Random Gif example

--- examples/random-gif-cell/src/random-gif-list/view.js
+++ examples/random-gif-cell/src/random-gif-list/view.js
@@ -2,7 +2,7 @@
 import m from "mithril"
 import { nest } from "meiosis-setup/mergerino"
 
-import { RandomGif } from "../random-gif"
+import { RandomGif, randomGif } from "../random-gif"
 import { buttonStyle } from "../util/ui"
 import { hasGifs } from "./util"
 
@@ -11,7 +11,7 @@ const RandomGifItem = {
     m(
       "div.dib.mr2",
       { key: subId },
-      m(RandomGif, { cell: nest(cell, subId) }),
+      m(RandomGif, { cell: nest(cell, subId, randomGif.Actions) }),
       m("button.bg-red" + buttonStyle, { onclick: () => cell.actions.remove(subId) }, "Remove")
     )
 }
