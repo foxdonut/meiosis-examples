1388fb81cc03319edbafd2e9eb2d2b13f09b93ba
foxdonut <fdaoud@fastmail.com>
2023-03-19 14:37:20 -0400

    Using MeiosisView<S> in Coffee Shop

--- examples/coffee-shop/src/app/view.tsx
+++ examples/coffee-shop/src/app/view.tsx
@@ -1,7 +1,5 @@
-import { MeiosisCell } from 'meiosis-setup/types';
+import { MeiosisView } from 'meiosis-setup/types';
 import { State } from '../types';
-
-type MeiosisView<S> = (params: { cell: MeiosisCell<S>, [others: string]: unknown }) => any;
 
 export const App: MeiosisView<State> = ({ cell, more }) => (
   <div>{cell.state}{more}</div>
