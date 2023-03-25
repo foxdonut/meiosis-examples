8a9d5711aa6452a6cf6b16949e5452d82608ae6f
foxdonut <fdaoud@fastmail.com>
2023-03-25 17:35:39 -0400

    working on coffee shop example

--- examples/coffee-shop/src/modal-example/view.tsx
+++ examples/coffee-shop/src/modal-example/view.tsx
@@ -2,6 +2,11 @@ import { MeiosisCell, MeiosisView } from 'meiosis-setup/types';
 import { State } from '../types';
 import { openModal } from '../common/modal';
 
+const actions = {
+  launchModalSm: (cell: MeiosisCell<State>) => openModal(cell, MyModal, 'modal-sm'),
+  launchModalLg: (cell: MeiosisCell<State>) => openModal(cell, MyModal, 'modal-lg')
+};
+
 const MyModal: MeiosisView<State> = ({ cell }) => (
   <div class="modal-content">
     <div class="modal-header">
@@ -21,12 +26,13 @@ const MyModal: MeiosisView<State> = ({ cell }) => (
   </div>
 );
 
-const actions = {
-  launchModal: (cell: MeiosisCell<State>) => openModal(cell, MyModal, 'modal-lg')
-};
-
 export const ModalButton: MeiosisView<State> = ({ cell }) => (
-  <button type="button" class="btn btn-primary" onClick={() => actions.launchModal(cell)}>
-    Launch example modal
-  </button>
+  <div>
+    <button type="button" class="btn btn-primary" onClick={() => actions.launchModalSm(cell)}>
+      Launch small modal
+    </button>
+    <button type="button" class="btn btn-primary ms-1" onClick={() => actions.launchModalLg(cell)}>
+      Launch large modal
+    </button>
+  </div>
 );
